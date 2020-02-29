9ec6bcc9c53046f94f786e894d29884d79bcc9e9
andrioy <andrioy@gmail.com>
2020-02-29 12:58:27 +0200

    final commit

--- src/com/vsc/Main.java
+++ src/com/vsc/Main.java
@@ -19,16 +19,16 @@ public class Main {
         mPlaces[date][hour] = null;
     }
 
-    public static void changePlace(String mPlace) {
+    public static String changePlace() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter new place for this meeting: ");
-        mPlace = input.nextLine();
-
+        return input.nextLine();
     }
 
     public static void printMeeting(int mFinalsHours, String mNames, String mComments, String mPlaces, int hour) {
-
-        System.out.println((hour + 8) + ":00h - " + mFinalsHours + ":00h : " + mNames + ", " + mPlaces + " Comment: " + mComments);
+        if (mFinalsHours == 0) System.out.println("Don`t have meeting to print!");
+        else
+            System.out.println((hour + 8) + ":00h - " + mFinalsHours + ":00h : " + mNames + ", " + mPlaces + " Comment: " + mComments);
     }
 
     public static int[] searchMeetings(String[][] mNames) {
@@ -38,7 +38,7 @@ public class Main {
         for (int i = 0; i < mNames.length; i++) {
             for (int j = 0; j < mNames[0].length; j++) {
                 if (meeting.equals(mNames[i][j])) {
-                    return new int[]{i, j + 8};
+                    return new int[]{i, j};
                 }
             }
 
@@ -49,14 +49,26 @@ public class Main {
 
     public static void searchFreeTimeToMeeting(int[][] mFinalsHours) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter date do you want your meeting: ");
-        int date = input.nextInt() - 1;
+
+
+        int date;
+        do {
+            System.out.print("Enter date do you want your meeting: ");
+            date = input.nextInt() - 1;
+        } while (date < 1 || date > 31);
         System.out.print("Enter duration of the meeting in hours: ");
         int duration = input.nextInt();
-        System.out.print("Which day of week is 1st date of this month?(1=Monday,..,7=Sunday)");
-        int dayOfWeek = input.nextInt();
-        System.out.print("How many days have in this month? ");
-        int monthLength = input.nextInt();
+        int dayOfWeek;
+        do {
+            System.out.print("Which day of week is 1st date of this month (1=Monday,..,7=Sunday)? ");
+            dayOfWeek = input.nextInt();
+        } while (dayOfWeek < 0 || dayOfWeek > 7);
+        int monthLength;
+        do {
+            System.out.print("How many days have in this month? ");
+            monthLength = input.nextInt();
+        } while (monthLength < 28 || monthLength > 31);
+
 
         int freeTime = 0;
         for (int i = date; i < monthLength; i++) {
@@ -64,7 +76,7 @@ public class Main {
                 if (mFinalsHours[i][j] == 0 && dayOfWeek != 6 && dayOfWeek != 7) {
                     freeTime++;
                     if (freeTime == duration) {
-                        System.out.println("Date: " + i + ", Hour: " + (j + 8) + ":00h");
+                        System.out.println("Date: " + i + "and hour: " + (j + 8) + ":00h is free.");
                     }
                 }
                 freeTime = 0;
@@ -78,14 +90,25 @@ public class Main {
     public static void addMeeting(int[][] mFinalsHours, String[][] mNames, String[][] mComments, String[][] mPlaces) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter date do you want your meeting: ");
-        int date = input.nextInt();
-        System.out.print("Enter hour do you want your meeting: ");
-        int hour = input.nextInt()-9;
-        System.out.print("Enter final hour of meeting: (last hour 17)");
-        mFinalsHours[date][hour] = input.nextInt();
+        int date;
+        do {
+            date = input.nextInt();
+        } while (date < 1 || date > 31);
+        input.nextLine();
+        int hour;
+        do {
+            System.out.print("Enter hour do you want your meeting (between 8-16): ");
+            hour = input.nextInt() - 8;
+        } while (hour < 0 || hour > 8);
+        mFinalsHours[date][hour] = -1;
+        while (mFinalsHours[date][hour] > 17 || mFinalsHours[date][hour] < 9) {
+            System.out.print("Enter final hour of meeting (between 9-17): ");
+            mFinalsHours[date][hour] = input.nextInt();
+        }
+        input.nextLine();
         System.out.print("Enter name of meeting: ");
         mNames[date][hour] = input.nextLine();
-        System.out.print("Enter meeting meeting place: ");
+        System.out.print("Enter meeting  place: ");
         mPlaces[date][hour] = input.nextLine();
         System.out.print("Enter meeting comment: ");
         mComments[date][hour] = input.nextLine();
@@ -96,33 +119,45 @@ public class Main {
     public static void removeMeeting(int[][] mFinalsHours, String[][] mNames, String[][] mComments, String[][] mPlaces) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter date do you want your meeting: ");
-        int date = input.nextInt();
-        System.out.print("Enter meeting hour which do you want to cancel: ");
-        int hour = input.nextInt();
+        int date;
+        do {
+            date = input.nextInt();
+        } while (date < 1 || date > 31);
+        printMeetings(mFinalsHours[date], mNames[date], mComments[date], mPlaces[date]);
+        input.nextLine();
+        System.out.print("Enter which meeting do you want to cancel: ");
+        String meet = input.nextLine();
+        int hour = 0;
+        for (int i = 0; i < mNames[0].length; i++) {
+            if (meet.equals(mNames[date][i])) {
+                mNames[date][i] = null;
+                hour = i;
+            }
+        }
         mFinalsHours[date][hour] = 0;
-        mNames[date][hour] = null;
         mComments[date][hour] = null;
         mPlaces[date][hour] = null;
     }
 
 
     public static void printMeetings(int[] mFinalsHours, String[] mNames, String[] mComments, String[] mPlaces) {
-        System.out.println("Start - Final :  Meeting,  Place\n------------------\n");
+        System.out.println("Start - Final :  Meeting,  Place\n------------------------------------\n");
         for (int i = 0; i < mFinalsHours.length; i++) {
-            System.out.println(i + 8 + ":00h - " + mFinalsHours[i] + ":00h : " + mNames[i] + ", " + mPlaces[i] + " Comment: " + mComments[i]);
+            if (mFinalsHours[i] != 0) {
+                System.out.println((i + 8) + ":00h - " + mFinalsHours[i] + ":00h : " + mNames[i] + ", " + mPlaces[i] + " Comment: " + mComments[i]);
+            }
         }
-
     }
 
     public static void menu(int[][] mFinalsHours, String[][] mNames, String[][] mComments, String[][] mPlaces) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Menu:\n----------------------------------------\n 1.Add or cancel a meeting.\n 2. Print meetings of a day.\n" +
+        System.out.print("Menu:\n----------------------------------------\n 1.Add or cancel a meeting.\n 2.Print meetings of a day.\n" +
                 " 3.Change meeting time or meeting place.\n 4.Search meeting.\n 5.Search free time for meeting.\n----------------------------------------\n" +
                 "Enter your choice: ");
         int choice = input.nextInt();
         switch (choice) {
             case 1:
-                System.out.println("If you want to add a meeting(true=add/false=remove)?");
+                System.out.print("Do you want to add a meeting ( true = add / false = remove ) ? ");
                 boolean add = input.nextBoolean();
                 if (add) addMeeting(mFinalsHours, mNames, mComments, mPlaces);
                 else removeMeeting(mFinalsHours, mNames, mComments, mPlaces);
@@ -139,7 +174,7 @@ public class Main {
                 if (change) {
                     changeHour(mFinalsHours, mNames, mComments, mPlaces, changeThis[0], changeThis[1]);
                     break;
-                } else changePlace(mPlaces[changeThis[0]][changeThis[1]]);
+                } else mPlaces[changeThis[0]][changeThis[1]] = changePlace();
                 break;
             case 4:
                 int[] m = searchMeetings(mNames);
@@ -152,20 +187,16 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
         int[][] meetingsFinalsHours = new int[31][8];
         String[][] meetingsNames = new String[31][8];
         String[][] meetingsComments = new String[31][8];
         String[][] meetingsPlaces = new String[31][8];
-
-        menu(meetingsFinalsHours, meetingsNames, meetingsComments, meetingsPlaces);
-
-
-        //  System.out.println("Enter date of month: ");
-        //   int date = input.nextInt();
-        //  System.out.println("Enter start hour of meeting: ");
-        //   int startHour = input.nextInt();
-        // addMeeting(finalsHours[date][startHour-9],meetingsNames[date][startHour-9],meetingsComments[date][startHour-9]);
-
+        boolean repeat;
+        do {
+            menu(meetingsFinalsHours, meetingsNames, meetingsComments, meetingsPlaces);
+            System.out.println("Do you want to continue(true = yes / false = no)? ");
+            repeat = input.nextBoolean();
+        } while (repeat);
     }
 }
